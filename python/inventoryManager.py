0f16596d4cdc25057127e85ae8c87e9f2be8e074
Seth Miller <sethmiller24@whitworth.edu>
2021-12-14 17:22:39 -0800

    experiment with inventory modificaitons

---
+++ python/inventoryManager.py
@@ -0,0 +1,56 @@
+""" deals with inventory management
+
+    Methods:
+        countTransactions()
+        modInventory()
+        createInventory()
+            DELETE FROM Inventory;
+            create crossproduct of prod and bank table
+                INSERT INTO Inventory
+                VALUES (id, bank, prod, 0);
+        
+        
+        
+"""
+
+import runSQL
+
+def countTransactions(prodID, bankID):
+    #Get the count of transactions of the current item at the specified bank
+count = runSQL.runSQL("SELECT Sum(T.Quantity) AS [Sum], T.Trans_Type, T.ProdID, T.LocID" +
+    +"FROM Transaction AS T, Inventory AS I\n"
+    +"WHERE (((T.ProdID)="+prodID+") AND ((T.LocID)=" +bankID+ "))\n"
+    +"GROUP BY T.Trans_Type, T.ProdID, T.LocID;")
+
+try:
+    count = count[0][0]
+except: #see if there exists a 
+    return False
+
+return count
+
+def modInventory(prodID, bankID):
+    #check if row exists with this number
+    
+    
+    runSQL.runSQL("UPDATE Inventory"+
+    "SET Quantity="+countTransactions(prodID,bankID)+""+
+    "WHERE ProdID="+prodID+" and BankID = "+bankID+";")
+    
+    return true
+
+def createInventory():
+            #empty any previous rows 
+            runSQL.runSQL("DELETE FROM Inventory;")
+
+            #create crossproduct of prod and bank table
+            numProds = runSQL.runSQL("SELECT COUNT(P.ID)"
+                +"\nFROM Products as P")
+            numBanks = = runSQL.runSQL("SELECT COUNT(L.ID)"
+                +"\nFROM Locations as L")
+            numLoops = 0
+            for (i in range(numBanks))
+                for (j in range (numProds))
+                    runSQL.runSQL("INSERT INTO Inventory"+
+                        "\nVALUES ("+numLoops+","+i+", "+j+", 0);")
+                    numLoops++
