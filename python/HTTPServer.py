ad99a65fed141f287db271fe8ab7f6421aa616c4
Tab-160 <43759848+Tab-160@users.noreply.github.com>
2021-12-14 22:38:27 -0800

    Update HTTPServer.py

--- python/HTTPServer.py
+++ python/HTTPServer.py
@@ -12,7 +12,7 @@ import fileIO
 import passwordManagement
 import HTTPResponse
 
-HOST = "127.0.0.1"  # localhost
+HOST = "10.200.100.92"  # localhost
 PORT = 50001        # Port to listen on
 DOMAIN = HOST + ":" + str(PORT)
 
