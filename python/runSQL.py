86b34784cfc585eeec0abe65705ca211de6e14e8
Tab-160 <43759848+Tab-160@users.noreply.github.com>
2021-12-16 00:32:13 -0800

    update inventory

--- python/runSQL.py
+++ python/runSQL.py
@@ -51,3 +51,50 @@ def popInventory():
             numLoops+=1
 
 
+
+def upAllInv():
+    """
+    Brute Force Runs through all the Transaction history to update the inventory quantity for all inventories
+    """
+    prods = runSQL("SELECT ID FROM Products")
+    banks = runSQL("SELECT ID FROM Locations")
+    #iterate through all combos of prods and locations
+    for i in banks:
+        for j in prods:
+            #get the net inventory of transactions
+            numQuant = str(countTrans(j[0],i[0]))
+            #use this to update the current inventory
+            runSQL("UPDATE Inventory SET Quantity ="+numQuant+" WHERE ProdId = "+str(j[0])+" AND BankID = "+str(i[0])+";")
+
+
+def countTrans(prodID, bankID):
+    """
+    Searches through Transactions in finalProject.mdb adding up donations and subtracting all
+    transactions to track an accurate inventory for the specified product at the specified location
+    Args:
+        prodID: the id of desired product to look for transactions
+        bankID: the id of desired location to look for transactions
+
+    Returns:
+        count: the stock of the desired inventory
+    """
+    #Get the count of transactions and donations of the current item at the specified bank
+    countDon = runSQL("SELECT Sum(T.Quantity) AS [Sum], T.Trans_Type, T.ProdID, T.LocID FROM Transaction AS T WHERE (((T.ProdID)="+str(prodID)+") AND (T.LocID)="+str(bankID)+") AND T.Trans_Type = 'Donation' GROUP BY T.Trans_Type, T.ProdID, T.LocID;")
+    countTrans = runSQL("SELECT Sum(T.Quantity) AS [Sum], T.Trans_Type, T.ProdID, T.LocID FROM Transaction AS T WHERE (((T.ProdID)="+str(prodID)+") AND (T.LocID)="+str(bankID)+") AND T.Trans_Type = 'Transaction' GROUP BY T.Trans_Type, T.ProdID, T.LocID;")
+   
+    #check whether these exists, else default to 0
+    try:
+        countDon = countDon[0][0]
+    except: #see if there exists any donation history
+        countDon = 0
+
+    try:
+        countTrans = countTrans[0][0]
+    except: #see if there exists any transaction history
+        countTrans = 0
+    #calc net inventory with donations increasing quantity and transactions depleting it
+    count = 0
+    count += countDon
+    count -= countTrans
+    return count
+
